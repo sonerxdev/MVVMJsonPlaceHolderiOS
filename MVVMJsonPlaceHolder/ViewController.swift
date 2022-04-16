740728136a3fa3725968230821bab730d68dd55a
sonerxdev <sonerkaraevli@gmail.com>
2022-04-16 18:46:29 +0300

    Initial Commit

---
+++ MVVMJsonPlaceHolder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMJsonPlaceHolder
+//
+//  Created by Soner Karaevli on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
